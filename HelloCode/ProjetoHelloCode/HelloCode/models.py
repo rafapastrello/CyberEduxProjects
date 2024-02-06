4248e84f4714fb65e918825dc4acbfccd57f2435
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2024-02-06 14:05:52 -0400

    update 06.05

---
+++ HelloCode/ProjetoHelloCode/HelloCode/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    publicado_em = models.DateTimeField(auto_now_add=True)
+    autor = models.CharField(max_length=100)
+    resumo = models.TextField()
+    slug = models.SlugField()
+    img_url = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-publicado_em']
--- HelloCode/ProjetoHelloCode/ProjetoHelloCode/asgi.py
+++ HelloCode/ProjetoHelloCode/ProjetoHelloCode/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HelloCode project.
+ASGI config for ProjetoHelloCode project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HelloCode.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjetoHelloCode.settings')
 
 application = get_asgi_application()
