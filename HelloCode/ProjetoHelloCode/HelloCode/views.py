955142c40371aa5c3d73f7be8c22db93fac7d5f0
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2024-02-07 15:26:27 -0400

    format document html

--- HelloCode/ProjetoHelloCode/HelloCode/views.py
+++ HelloCode/ProjetoHelloCode/HelloCode/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def minha_view(request):
+    context = {'mensagem': 'Olá, Mundo!'}
+    return render(request, 'meu_template.html', context)
