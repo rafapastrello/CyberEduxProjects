2eef7d7e4c2ef1f9f52cd883f7be3b5d495211f3
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2024-02-16 16:10:00 -0400

    update

--- exercicios_filipe/16-02-24/teste_login/geral/views.py
+++ exercicios_filipe/16-02-24/teste_login/geral/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'incorrect_login': False
+        })
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password= request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/userspace')
+        else:
+            return render(request, 'login.html', {
+                'incorrect_login': True
+            })
+    else:
+        return HttpResponseBadRequest()
 
-
+@login_required(login_url='/login')
 def userspace_view(request):
-    return render(request, 'userspace.html')
+    return render(request, 'userspace.html', {
+        'username': request.user.username
+    })
