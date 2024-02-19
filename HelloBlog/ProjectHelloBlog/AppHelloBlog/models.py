d7e487436a0a8a2d3e03c996b69ad50976781a0e
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2024-02-19 14:43:25 -0400

    update

--- HelloBlog/ProjectHelloBlog/AppHelloBlog/models.py
+++ HelloBlog/ProjectHelloBlog/AppHelloBlog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    autor = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    dt_publicacao = models.DateTimeField(auto_now_add=True)
+    resumo = models.TextField()
+    slug = models.SlugField()
+    img_url = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-dt_publicacao']
