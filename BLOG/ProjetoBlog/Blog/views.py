7f4d68d4990490b35d54f597e1cb7724b57bf8d1
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2024-02-19 15:26:48 -0400

    update

--- BLOG/ProjetoBlog/Blog/views.py
+++ BLOG/ProjetoBlog/Blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Post, Comentario
 
 def home(request):
--- HelloBlog/ProjectHelloBlog/AppHelloBlog/views.py
+++ HelloBlog/ProjectHelloBlog/AppHelloBlog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from models import Post
 
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    for post in posts:
+        post.nome_img = '/AppHelloBlog/img/thumbnails/' + post.nome_img
+    return render(request, 'home.html', {'posts': posts})
